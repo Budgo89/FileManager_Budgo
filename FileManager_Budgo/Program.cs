8083b8e6c390243306376fda3b50d0123db9087e
Budgo <Budgo@mail.ru>
2021-03-19 14:59:50 +0300

    Создание проекта

---
+++ FileManager_Budgo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileManager_Budgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
